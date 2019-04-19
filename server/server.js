ec75b9bb91c0a6bfcfb718e9ee22453f313bc213
A1webtechies <qalibabbasf18@gmail.com>
2019-04-20 03:52:31 +0500

    =Port is Chnaged

--- server/server.js
+++ server/server.js
@@ -5,6 +5,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
