e137a4589a7ef5959feec1fca7372f445aa68bd4
A1webtechies <qalibabbasf18@gmail.com>
2019-04-23 03:46:01 +0500

    Basic UI set up

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,30 @@
 var socket = io();
 socket.on("connect", function() {
   console.log("Contected To Server");
+  if (jQuery) {
+    console.log("Jquery working");
+  }
 });
 socket.on("disconnect", function() {
   console.log("Server disconnected");
 });
 socket.on("newMessage", function(Message) {
   console.log("new Message ", Message);
+  var li = $("<li></li>");
+  li.text(`${Message.from}: ${Message.text}`);
+  $("#message").append(li);
 });
-socket.emit(
-  "createMessage",
-  {
-    from: "Qalib",
-    text: "Hey"
-  },
-  function(data) {
-    console.log("Got it", data);
-  }
-);
+
+$("#message-form").on("submit", function(e) {
+  e.preventDefault();
+  socket.emit(
+    "createMessage",
+    {
+      from: "User",
+      text: $("[name=message]").val()
+    },
+    function(data) {
+      console.log("Got It", data);
+    }
+  );
+});
